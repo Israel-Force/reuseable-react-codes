2050a178a3cdcebbddd60738b5224bdb9d7d24da
israel-force <oladosuisraelolayinka@gmail.com>
2020-11-23 10:09:25 +0100

    added my reuseable codes

---
+++ src/config/colors.js
@@ -0,0 +1,28 @@
+const colors = {
+  borderColor: '#A9ABAE',
+  buttonBackground: '#FFCC29',
+  buttonColor: '#a0d9d9',
+  dashboardBg: '#E4F0F6',
+  colorBlack: '#000',
+  colorDeepBlue: '#2582D2',
+  colorGrey: '#ddd',
+  colorGreyShade1: '#e6e7e8',
+  colorGreyDark: '#4E5A62',
+  colorGreyLight: '#fafafa',
+  colorGreen: '#00A859',
+  colorLightGreen: '#91D8F7',
+  colorLightBlue: '#0098DA',
+  colorOrange: '#ED642B',
+  colorPink: '#FCF8F5',
+  colorRed: '#ED3237',
+  colorWhite: '#fff',
+  colorOcean: '#889FAC',
+  colorSky: '#C9D8EB',
+  colorEqi: '#F2F5FA',
+  footer: '#F5F6F8',
+  navBarBlue: '#243137',
+  tabBorder: '#A7C4D2',
+  textColor: '#4E5A62',
+};
+
+export default colors;
